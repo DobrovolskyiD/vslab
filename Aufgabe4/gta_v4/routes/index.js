14855cd137075c3c500f1b4aa07da49fc1428784
Dobrovolskyi2003 <dimadobr2@gmail.com>
2024-06-28 04:05:55 +0200

    1st gen pagination , do we need to have it while sorted??

--- Aufgabe4/gta_v4/routes/index.js
+++ Aufgabe4/gta_v4/routes/index.js
@@ -1,126 +1,128 @@
-// File origin: VS1LAB A3, A4
-
-/**
- * This script defines the main router of the GeoTag server.
- * It's a template for exercise VS1lab/Aufgabe3
- * Complete all TODOs in the code documentation.
- */
-
-/**
- * Define module dependencies.
- */
-
 const express = require('express');
 const router = express.Router();
-
-/**
- * The module "geotag" exports a class GeoTagStore. 
- * It represents geotags.
- */
-// eslint-disable-next-line no-unused-vars
 const GeoTag = require('../models/geotag');
-
-/**
- * The module "geotag-store" exports a class GeoTagStore. 
- * It provides an in-memory store for geotag objects.
- */
-// eslint-disable-next-line no-unused-vars
 const InMemoryGeoTagStore = require('../models/geotag-store');
-
-// App routes (A3)
-
+let storedTags = [];
 const store = new InMemoryGeoTagStore();
-
-// Load example GeoTags into the store
-store.populateWithExamples(); // Call this method to load example GeoTags into the store
-//const tags = store.getGeoTags(); // Holen Sie sich alle GeoTags
+store.populateWithExamples();
 
 
+const ITEMS_PER_PAGE = 5;
 
-/**
- * Route '/' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
- *
- * Requests cary no parameters
- *
- * As response, the ejs-template is rendered without geotag objects.
- */
-
-// TODO: extend the following route example if necessary
 router.get('/', (req, res) => {
-  res.render('index', { taglist: [], latitude: null  , longitude : null  })
+  const { latitude, longitude, searchterm, page } = req.query;
+  let results = store.getGeoTags();
+
+  if (searchterm) {
+    results = store.searchGeoTags(searchterm);
+  } else if (latitude && longitude) {
+    results = store.getNearbyGeoTags(parseFloat(latitude), parseFloat(longitude), 100); // Adjust radius as needed
+  }
+
+  const totalItems = results.length;
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+  let currentPage = parseInt(page, 10) || 1;
+
+  // Ensure currentPage is within valid range
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > totalPages) {
+    currentPage = totalPages;
+  }
+
+  const start = (currentPage - 1) * ITEMS_PER_PAGE;
+  const paginatedResults = results.slice(start, start + ITEMS_PER_PAGE);
+
+  res.render('index', {
+    taglist: paginatedResults,
+    latitude: latitude || null,
+    longitude: longitude || null,
+    searchterm: searchterm || '',
+    currentPage: currentPage,
+    totalPages: totalPages
+  });
 });
-
-
-/**
- * Route '/tagging' for HTTP 'POST' requests.
- * (http://expressjs.com/de/4x/api.html#app.post.method)
- *
- * Requests cary the fields of the tagging form in the body.
- * (http://expressjs.com/de/4x/api.html#req.body)
- *
- * Based on the form data, a new geotag is created and stored.
- *
- * As response, the ejs-template is rendered with geotag objects.
- * All result objects are located in the proximity of the new geotag.
- * To this end, "GeoTagStore" provides a method to search geotags 
- * by radius around a given location.
- */
 
 router.post('/tagging', (req, res) => {
   const { name, latitude, longitude, hashtag } = req.body;
-  const newGeoTag = new GeoTag(name, latitude, longitude, hashtag); 
+  const newGeoTag = new GeoTag(name, latitude, longitude, hashtag);
   store.addGeoTag(newGeoTag);
+
   const nearbyTags = store.getNearbyGeoTags(newGeoTag.latitude, newGeoTag.longitude, 100); // Adjust radius as needed
-  res.render('index', { taglist: nearbyTags,latitude: latitude , longitude : longitude });
+  const totalItems = nearbyTags.length;
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+  const currentPage = parseInt(req.query.page) || 1;
+
+  const start = (currentPage - 1) * ITEMS_PER_PAGE;
+  const paginatedResults = nearbyTags.slice(start, start + ITEMS_PER_PAGE);
+  
+  res.render('index', {
+    taglist: paginatedResults,
+    latitude: latitude,
+    longitude: longitude,
+    currentPage: currentPage,
+    totalPages: totalPages
+  });
 });
-
-
-/**
- * Route '/discovery' for HTTP 'POST' requests.
- * (http://expressjs.com/de/4x/api.html#app.post.method)
- *
- * Requests cary the fields of the discovery form in the body.
- * This includes coordinates and an optional search term.
- * (http://expressjs.com/de/4x/api.html#req.body)
- *
- * As response, the ejs-template is rendered with geotag objects.
- * All result objects are located in the proximity of the given coordinates.
- * If a search term is given, the results are further filtered to contain 
- * the term as a part of their names or hashtags. 
- * To this end, "GeoTagStore" provides methods to search geotags 
- * by radius and keyword.
- */
 
 router.post('/discovery', (req, res) => {
   const { latitude, longitude, searchterm } = req.body;
+ 
   let results;
   if (searchterm) {
-    results = store.searchNearbyGeoTags(searchterm, latitude, longitude, 100); // Adjust radius as needed
+    results = store.searchNearbyGeoTags(searchterm, latitude, longitude, 100);
+    
   } else {
-    results = store.getNearbyGeoTags(latitude, longitude, 100); // Adjust radius as needed
+    results = store.getNearbyGeoTags(latitude, longitude, 100);
+    
   }
-  res.render('index', { taglist: results,latitude: latitude , longitude : longitude  });
+
+  const totalItems = results.length;
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+  let currentPage = parseInt(req.query.page) || 1;
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > totalPages) {
+    currentPage = totalPages;
+  }
+  let start = (currentPage - 1) * ITEMS_PER_PAGE;
+  let paginatedResults = results.slice(start, start + ITEMS_PER_PAGE);
+
+  res.render('index', {
+    taglist: paginatedResults,
+    latitude: latitude,
+    longitude: longitude,
+    currentPage: currentPage,
+    totalPages: totalPages
+  });
 });
 
+router.get('/discovery', (req, res) => {
+  const { latitude, longtitude ,searchterm} = req.query;
+  let results = store.getGeoTags();
+  
+  const totalItems = results.length;
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+  let currentPage = parseInt(req.query.page) || 1;
 
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > totalPages) {
+    currentPage = totalPages;
+  }
 
+  let start = (currentPage - 1) * ITEMS_PER_PAGE;
+  let paginatedResults = results.slice(start, start + ITEMS_PER_PAGE);
 
-// API routes (A4)
+  res.render('index', {
+    taglist: paginatedResults,
+    latitude: latitude || null,
+    longitude: longtitude || null,
+    currentPage: currentPage,
+    totalPages: totalPages
+  });
+});
 
-/**
- * Route '/api/geotags' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
- *
- * Requests contain the fields of the Discovery form as query.
- * (http://expressjs.com/de/4x/api.html#req.query)
- *
- * As a response, an array with Geo Tag objects is rendered as JSON.
- * If 'searchterm' is present, it will be filtered by search term.
- * If 'latitude' and 'longitude' are available, it will be further filtered based on radius.
- */
-
-// TODO: ... your code here ...
 router.get('/api/geotags', (req, res) => {
   const { latitude, longitude, searchterm } = req.query;
   let results;
@@ -134,18 +136,6 @@ router.get('/api/geotags', (req, res) => {
   res.json(results);
 });
 
-/**
- * Route '/api/geotags' for HTTP 'POST' requests.
- * (http://expressjs.com/de/4x/api.html#app.post.method)
- *
- * Requests contain a GeoTag as JSON in the body.
- * (http://expressjs.com/de/4x/api.html#req.body)
- *
- * The URL of the new resource is returned in the header as a response.
- * The new resource is rendered as JSON in the response.
- */
-
-// TODO: ... your code here ...
 router.post('/api/geotags', (req, res) => {
   const { name, latitude, longitude, hashtag } = req.body;
   const newGeoTag = new GeoTag(name, latitude, longitude, hashtag);
@@ -153,17 +143,6 @@ router.post('/api/geotags', (req, res) => {
   res.status(201).json(newGeoTag);
 });
 
-/**
- * Route '/api/geotags/:id' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
- *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
- *
- * The requested tag is rendered as JSON in the response.
- */
-
-// TODO: ... your code here ...
 router.get('/api/geotags/:id', (req, res) => {
   const id = req.params.id;
   const tag = store.getGeoTagById(id);
@@ -174,21 +153,6 @@ router.get('/api/geotags/:id', (req, res) => {
   }
 });
 
-/**
- * Route '/api/geotags/:id' for HTTP 'PUT' requests.
- * (http://expressjs.com/de/4x/api.html#app.put.method)
- *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
- * 
- * Requests contain a GeoTag as JSON in the body.
- * (http://expressjs.com/de/4x/api.html#req.query)
- *
- * Changes the tag with the corresponding ID to the sent value.
- * The updated resource is rendered as JSON in the response. 
- */
-
-// TODO: ... your code here ...
 router.put('/api/geotags/:id', (req, res) => {
   const id = req.params.id;
   const { name, latitude, longitude, hashtag } = req.body;
@@ -201,28 +165,14 @@ router.put('/api/geotags/:id', (req, res) => {
   }
 });
 
-/**
- * Route '/api/geotags/:id' for HTTP 'DELETE' requests.
- * (http://expressjs.com/de/4x/api.html#app.delete.method)
- *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
- *
- * Deletes the tag with the corresponding ID.
- * The deleted resource is rendered as JSON in the response.
- */
-
-// TODO: ... your code here ...
 router.delete('/api/geotags/:id', (req, res) => {
   const id = req.params.id;
-  const result = store.removeid(id);
+  const result = store.removeGeoTag(id);
   if (result) {
     res.json(result);
   } else {
     res.status(404).send('GeoTag not found');
   }
 });
-
-
 
 module.exports = router;
